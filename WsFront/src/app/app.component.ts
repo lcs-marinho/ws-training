9c5eacf7079c292f7489142398698549ce92d5b6
Lucas Neyson <126470162+LucasNeyson25@users.noreply.github.com>
2023-05-01 02:27:45 -0300

    feat: simple chat

--- WsFront/src/app/app.component.ts
+++ WsFront/src/app/app.component.ts
@@ -1,39 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 import { WebSocketService } from './web-socket-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'WsFront';
 
   messages: any[] = [];
   msg?: string;
-  name?: string;
+  name?: any = 'Qualquer nome';
   private subscription: any;
 
-  constructor(private wsService: WebSocketService){}
+  constructor(private wsService: WebSocketService) {}
 
   ngOnInit() {
     this.subscription = this.wsService.connect().subscribe((message: any) => {
-      // this.messages.push(JSON.parse(message.body).message);
-      const msg = JSON.parse(message.body)
-      
+      this.messages.push(JSON.parse(message.body));
     });
+  
+    sessionStorage.setItem('username', this.name);
   }
 
-  public sendMessage(message: any): void {
-    this.wsService.sendMessage({username: this.name, message: this.msg});
+  sendMessage(el: HTMLInputElement): void {
+    el.value != ''
+      ? this.wsService.sendMessage({ username: this.name, message: el.value })
+      : '';
+    el.value = '';
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  show(){
-    console.log(this.messages)
+  show() {
+    console.log(this.messages);
   }
-
 }
--- WsFront/src/app/app.module.ts
+++ WsFront/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
